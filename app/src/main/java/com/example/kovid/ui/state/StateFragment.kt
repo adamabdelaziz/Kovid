c334844f1ddfaed1f101c8a41baa2dd6353e84b5
Adam <46168188+adamswagem@users.noreply.github.com>
2020-10-22 22:29:27 -0400

    State Fragment graphing WIP

--- app/src/main/java/com/example/kovid/ui/state/StateFragment.kt
+++ app/src/main/java/com/example/kovid/ui/state/StateFragment.kt
@@ -1,16 +1,25 @@
 package com.example.kovid.ui.state
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import com.example.kovid.MainActivity
+import com.example.kovid.R
 import com.example.kovid.data.entities.StateValue
 import com.example.kovid.databinding.FragmentStateBinding
 import com.example.kovid.utils.Resource
+import com.yabu.livechart.model.DataPoint
+import com.yabu.livechart.model.Dataset
+import com.yabu.livechart.view.LiveChart
+import com.yabu.livechart.view.LiveChartStyle
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_state.*
 import timber.log.Timber
 
 @AndroidEntryPoint
@@ -50,23 +59,23 @@ class StateFragment : Fragment() {
                 Resource.Status.SUCCESS -> {
                     Timber.d("historic data success")
 
-                    var list = it.data
+                    val list = it.data
+
                     if (list != null) {
                         Timber.d("historic list not null")
-                        for (i in list) {
-                            Timber.d(i.date.toString())
-                            Timber.d(list.size.toString())
-                            Timber.d(i.state)
+                        Timber.d(list.size.toString())
+                        if (isAdded) {
+                            setupChart(list)
                         }
+
                     }
+
                 }
                 Resource.Status.ERROR -> {
                     Timber.d("historic data error")
-
                 }
                 Resource.Status.LOADING -> {
                     Timber.d("historic data loading")
-
                 }
             }
         })
@@ -78,15 +87,17 @@ class StateFragment : Fragment() {
                 Resource.Status.SUCCESS -> {
                     Timber.d("sssss metadata SUCCESS")
                     binding.progressBar.visibility = View.GONE
-                    binding.stateConstraintLayout.visibility = View.VISIBLE
+                    //binding.gridLayout.visibility = View.VISIBLE
 
-                    //using the 2 letter state ID passed in from StateListFragment, get the actual data you need pertaining to the state and bind it in bindData()
-                    var list = it.data
+                    //using the 2 letter state ID passed in from StateListFragment,
+                    // get the actual data you need pertaining to the state and bind it in bindData()
+
+                    val list = it.data
                     if (list != null) {
                         Timber.d("list not null")
                         for (i in list) {
                             if (i.state == stateID) {
-                                Timber.d("matching state " + i.state)
+                                Timber.d("matching state%s", i.state)
                                 bindData(i)
 
                             }
@@ -95,39 +106,84 @@ class StateFragment : Fragment() {
                 }
 
                 Resource.Status.ERROR -> {
-                    Timber.d("ssss metadata  ERROR")
+                    Timber.d("stateFragment metadata  ERROR")
 
                 }
                 Resource.Status.LOADING -> {
-                    Timber.d("ssss metadata LOADING")
+                    Timber.d("stateFragment metadata LOADING")
                     binding.progressBar.visibility = View.VISIBLE
-                    binding.stateConstraintLayout.visibility = View.GONE
+                   // binding.gridLayout.visibility = View.GONE
                 }
             }
         })
     }
 
-    private fun setupChart(stateValue: StateValue) {
-//        val liveChart = binding.liveChart
-//
-//        val dataset = Dataset(
-//            mutableListOf(
-//                DataPoint(0f, 1f), DataPoint(1f, 3f),
-//                DataPoint(2f, 6f)
-//            )
-//        )
-//
-//        liveChart.setDataset(dataset)
-//            .drawYBounds()
-//            .drawBaseline()
-//            .drawFill()
-//            .drawDataset()
+    private fun setupChart(stateValueList: List<StateValue>) {
+        val liveChart = binding.liveChart
+        val positives = mutableListOf<DataPoint>()
+        val negatives = mutableListOf<DataPoint>()
+        positives.clear()
+        negatives.clear()
+        Timber.d("setup chart called")
+
+        // need to go through the list from the back
+        val stateList = stateValueList.reversed()
+
+        for ((index, value) in stateList.withIndex()) {
+
+            if(value.positive != null && value.negative != null)
+            {
+                val dataPointPos = DataPoint(index.toFloat(), value.positive.toFloat())
+                val dataPointNeg = DataPoint(index.toFloat(), value.negative.toFloat())
+                positives.add(dataPointPos)
+                negatives.add(dataPointNeg)
+            }
+
+            Timber.d(index.toString() + " index")
+            Timber.d(value.positive.toString() + " positive")
+            Timber.d(value.negative.toString() + " negative")
+            Timber.d(positives.size.toString() + " positives list size")
+            Timber.d(negatives.size.toString() + " negatives list size")
+
+        }
+
+        val positiveDataset = Dataset(positives)
+        val negativeDataset = Dataset(negatives)
+
+        val chartStyle = LiveChartStyle().apply {
+            mainColor = Color.RED
+            pathStrokeWidth = 8f
+            secondColor = Color.CYAN
+            secondPathStrokeWidth = 8f
+            textColor=ContextCompat.getColor(requireContext(),R.color.colorText)
+        }
+
+        liveChart.setDataset(positiveDataset)
+            .setLiveChartStyle(chartStyle)
+            .drawBaseline()
+            .drawYBounds()
+            .drawFill(true)
+            //.setSecondDataset(negativeDataset)
+            .drawSmoothPath()
+            .drawVerticalGuidelines(steps=4)
+            .drawHorizontalGuidelines(steps=4)
+            .setOnTouchCallbackListener(object : LiveChart.OnTouchCallback{
+                override fun onTouchCallback(point: DataPoint) {
+
+                }
+
+                override fun onTouchFinished() {
+
+                }
+            })
+            .drawDataset()
     }
 
     private fun bindData(stateValue: StateValue) {
         Timber.d("bindData called")
         Timber.d(stateValue.hash)
         Timber.d(stateValue.state)
+
 
 
     }

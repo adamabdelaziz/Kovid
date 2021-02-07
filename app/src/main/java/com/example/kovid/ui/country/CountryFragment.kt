d6904f7cfda1be337141c42ccf505a6d473f7d2e
Adam <46168188+adamswagem@users.noreply.github.com>
2021-02-06 20:15:00 -0500

    Country Fragment Graph Fix
    .reversed() not necessary

--- app/src/main/java/com/example/kovid/ui/country/CountryFragment.kt
+++ app/src/main/java/com/example/kovid/ui/country/CountryFragment.kt
@@ -1,17 +1,24 @@
 package com.example.kovid.ui.country
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import com.example.kovid.R
 import com.example.kovid.data.entities.USValue
 import com.example.kovid.databinding.FragmentCountryBinding
 import com.example.kovid.utils.Resource
 import com.yabu.livechart.model.DataPoint
+import com.yabu.livechart.model.Dataset
+import com.yabu.livechart.view.LiveChart
+import com.yabu.livechart.view.LiveChartStyle
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_state.*
 import timber.log.Timber
 
 @AndroidEntryPoint
@@ -93,12 +100,84 @@ class CountryFragment : Fragment() {
         val liveChartThree = binding.liveChartThree
 
         val positives = mutableListOf<DataPoint>()
-        val hospitializations = mutableListOf<DataPoint>()
+        val hospitalizations = mutableListOf<DataPoint>()
         val deaths = mutableListOf<DataPoint>()
 
         positives.clear()
-        hospitializations.clear()
+        hospitalizations.clear()
         deaths.clear()
+
+        //reversed() puts it in the correct order chronologically
+        //API doesnt require reversed() for historicUS data
+        //drop(n-90) gives the past 90 days only
+        val usList = historicUSValues.drop(historicUSValues.size - 89)
+
+        for ((index, value) in usList.withIndex()) {
+
+            if (value.positive != null && value.totalTestResults != null && value.hospitalizedCurrently != null && value.death != null) {
+                val dataPointPos = DataPoint(index.toFloat(), value.positive.toFloat())
+                val dataPointHosp =
+                    DataPoint(index.toFloat(), value.hospitalizedCurrently.toFloat())
+                val dataPointDeath = DataPoint(index.toFloat(), value.death.toFloat())
+                Timber.d(index.toString() + " TRUE INDEX??")
+
+                positives.add(dataPointPos)
+                hospitalizations.add(dataPointHosp)
+                deaths.add(dataPointDeath)
+            }
+        }
+
+        val positiveDataset = Dataset(positives)
+        val hospitalizationDataset = Dataset(hospitalizations)
+        val deathDataset = Dataset(deaths)
+
+        val chartStyle = LiveChartStyle().apply {
+            mainColor = ContextCompat.getColor(requireContext(), R.color.colorPrimary)
+            pathStrokeWidth = 8f
+            secondColor = Color.CYAN
+            secondPathStrokeWidth = 8f
+            textColor = ContextCompat.getColor(requireContext(), R.color.colorText)
+        }
+
+        liveChart.setDataset(positiveDataset)
+            .setLiveChartStyle(chartStyle)
+            .drawBaselineFromFirstPoint()
+            .drawFill(true)
+            .drawSmoothPath()
+            .drawVerticalGuidelines(steps = 4)
+            .drawHorizontalGuidelines(steps = 4)
+            .setOnTouchCallbackListener(object : LiveChart.OnTouchCallback {
+                override fun onTouchCallback(point: DataPoint) {
+                    //Display specific data here based on what point the user touches
+                    liveChart
+                        .parent
+                        .requestDisallowInterceptTouchEvent(true)
+
+                    Timber.d(point.x.toString() + " x")
+                    Timber.d(point.y.toString() + " y")
+                    Timber.d(usList[point.x.toInt()].date.toString())
+                    Timber.d(usList[point.x.toInt()].positive.toString())
+
+                    val value = usList[point.x.toInt()]
+
+                    bottom_body_four.text = value.date.toString()
+                    bottom_body_three.text = value.positiveIncrease.toString()
+                    if (value.death.toString() != "null" || value.death != null) {
+                        bottom_body_two.text = value.death.toString()
+                    } else {
+                        bottom_body_two.text = "0"
+                    }
+                    bottom_body_one.text =
+                        value.positive.toString() + "(" + value.totalTestResults.toString() + ")"
+
+
+                }
+
+                override fun onTouchFinished() {
+                    liveChart.parent.requestDisallowInterceptTouchEvent(true)
+                }
+            })
+            .drawDataset()
     }
 
     private fun bindData(USValue: USValue) {
